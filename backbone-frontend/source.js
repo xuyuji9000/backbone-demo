8ec327bf793c2d13fcbad43df085761213e9f0f2
Karl Xu <karl.yogi.xu@gmail.com>
2017-10-09 09:02:55 +0800

    get users collection

--- backbone-frontend/source.js
+++ backbone-frontend/source.js
@@ -1,1 +1,27 @@
-console.log('test')
+var User = Backbone.Model.extend({
+  defaults: {
+    id: '',
+    name: ''
+  }
+});
+var Users = Backbone.Collection.extend({
+  model: User,
+  url: 'http://localhost:9001/users'
+});
+
+var users = new Users;
+
+users.fetch();
+
+var AppView = Backbone.View.extend({
+  el: '#container',
+  initialize: function() {
+    this.render();
+  },
+  render: function() {
+    this.$el.empty();
+    this.$el.html("Hello World");
+  }
+});
+
+var appView = new AppView();
--- php-backend/config/middleware.php
+++ php-backend/config/middleware.php
@@ -4,6 +4,6 @@
 // Cross Domain Access
 $app->add(function($request, $response, $next){
     $response = $next($request, $response);
-    $response = $response->withHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+    $response = $response->withHeader('Access-Control-Allow-Origin', 'http://localhost:9002');
     return $response;
 });
